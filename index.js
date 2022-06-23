c35005b7e694a8e91d531dd627dd77774643d012
ElisaSalvatore <elisa.salvatore110@gmail.it>
2022-06-23 19:34:56 +0200

    use route params

--- index.js
+++ index.js
@@ -4,11 +4,35 @@ const express = require('express');
 const app = express();
 
 /** Importo il modulo persone */
-const {persone} = require('./persone');
+const { persone } = require('./persone');
 
-/**Creo un JSON */
+/**Routes param*/
 app.get('/', (req, res) => {
-    res.json(persone)
+    res.sendFile('homepage.html', { root: __dirname + '/public' });
+})
+
+app.get('/persone', (req, res) => {
+    const nuovePersone = persone.map((persona) => {
+        const { nome, cognome, eta } = persona;
+        return { nome, cognome, eta }
+    })
+    res.json(nuovePersone);
+})
+
+app.get('/persone/:id', (req, res) => {
+    console.log(req.params);
+
+    const id = req.params.id;
+    const persona = persone.find((persona) => {
+        return persona.id === id;
+    })
+    //const persona = persone.find((persona) => persona.id === id);
+
+    if(!persona) {
+        return res.status(404).json({messaggio: "Persona non trovata", code: 404});
+    }
+
+    res.json(persona);
 })
 
 /** listen() per server in ascolto */
