1658701e18f2aae36fdedb4ce49986b8bda84c5f
ElisaSalvatore <elisa.salvatore110@gmail.it>
2022-06-24 18:44:58 +0200

    testing methods and api

--- persone.js
+++ persone.js
@@ -3,61 +3,61 @@ const persone = [
         id: "1",
         nome: "Mario",
         congnome: "Rossi",
-        eta: 24,
-        indirizzo: {
-            citta: "Torino",
-            via: "via Milano, 43",
-            cap: "34567"
-        },
-        interessi: ["basket", "calcio"]
+        // eta: 24,
+        // indirizzo: {
+        //     citta: "Torino",
+        //     via: "via Milano, 43",
+        //     cap: "34567"
+        // },
+        // interessi: ["basket", "calcio"]
     },
     {
         id: "2",
         nome: "Anna",
         congnome: "Verdi",
-        eta: 24,
-        indirizzo: {
-            citta: "Milano",
-            via: "via Roma, 4",
-            cap: "98765"
-        },
-        interessi: ["pallamano", "rugby"]
+        // eta: 24,
+        // indirizzo: {
+        //     citta: "Milano",
+        //     via: "via Roma, 4",
+        //     cap: "98765"
+        // },
+        // interessi: ["pallamano", "rugby"]
     },
     {
         id: "3",
         nome: "Luca",
         congnome: "Franceschi",
-        eta: 24,
-        indirizzo: {
-            citta: "Milano",
-            via: "via Roma, 4",
-            cap: "98765"
-        },
-        interessi: ["pallamano", "rugby"]
+        // eta: 24,
+        // indirizzo: {
+        //     citta: "Milano",
+        //     via: "via Roma, 4",
+        //     cap: "98765"
+        // },
+        // interessi: ["pallamano", "rugby"]
     },
     {
         id: "4",
         nome: "Marta",
         congnome: "Pizzicotto",
-        eta: 24,
-        indirizzo: {
-            citta: "Milano",
-            via: "via Roma, 4",
-            cap: "98765"
-        },
-        interessi: ["pallamano", "rugby"]
+        // eta: 24,
+        // indirizzo: {
+        //     citta: "Milano",
+        //     via: "via Roma, 4",
+        //     cap: "98765"
+        // },
+        // interessi: ["pallamano", "rugby"]
     },
     {
         id: "5",
         nome: "Ludovica",
         congnome: "Viola",
-        eta: 24,
-        indirizzo: {
-            citta: "Milano",
-            via: "via Roma, 4",
-            cap: "98765"
-        },
-        interessi: ["pallamano", "rugby"]
+        // eta: 24,
+        // indirizzo: {
+        //     citta: "Milano",
+        //     via: "via Roma, 4",
+        //     cap: "98765"
+        // },
+        // interessi: ["pallamano", "rugby"]
     },
 ]
 
